ba91b6a903a635cf682e6a33e87ff9c81822364e
Shihab Khan (ext 1112) <skhan@lxa.on.ca>
2019-04-08 18:20:47 -0400

    add bootstrap styles to audit page

--- mainforms/templates/mainforms/signals.py
+++ mainforms/templates/mainforms/signals.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.signals import user_logged_out
-from django.contrib import messages
+
+#from django.contrib.auth.signals import user_logged_out
+#from django.contrib import messages
 
 
-def show_message(sender, user, request, **kwargs):
-    # whatever...
-    messages.info(request, 'You have been logged out.')
+# def show_message(sender, user, request, **kwargs):
+# whatever...
+#   messages.info(request, 'You have been logged out.')
 
-user_logged_out.connect(show_message)
+# user_logged_out.connect(show_message)
